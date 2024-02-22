47a43e3fc93dd60c908596e5186c49b8061f1965
HuynhPhuongAnh_Nguyen <phnganhnguyn0323@gmail.com>
2024-02-22 22:47:41 +1100

    all field finished!

--- My project/Assets/AudioManager.cs
+++ My project/Assets/AudioManager.cs
@@ -13,16 +13,19 @@ public class AudioManager : MonoBehaviour
         if (instance == null)
             instance = this;
         else{
+            Debug.Log("Duplicate AudioManager found and destroyed.");
             Destroy(gameObject);
             return;
         }
         DontDestroyOnLoad(gameObject);
+        
         foreach (Sound s in sounds){
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
+            Debug.Log($"Created AudioSource for sound: {s.name}");
 
         }
     }
@@ -35,6 +38,11 @@ public class AudioManager : MonoBehaviour
         if (s == null)
         {
             Debug.LogWarning("Sound: " + name + "not found!");
+            return;
+        }
+        if (s.source == null)
+        {
+            Debug.LogError($"AudioSource for {name} is null.");
             return;
         }
         s.source.Play();
--- My project/Assets/SoldierManager.cs
+++ My project/Assets/SoldierManager.cs
@@ -7,7 +7,9 @@ using UnityEngine;
 public class SoldierManager : MonoBehaviour
 {
     public int soldierCount;
+    public int soldierTreated;
     public PlayerCollision playerCollision;
+    public GameManager gameManager;
     public TextMeshProUGUI soldierCountText;
 
     // Update is called once per frame
@@ -25,6 +27,10 @@ public class SoldierManager : MonoBehaviour
         else if (soldierCount ==0) {
         soldierCountText.text = "Soldier Rescued: " + soldierCount.ToString() + "/3"; 
 
+        }
+
+        if (soldierTreated >= 4){
+            gameManager.youWin();
         }
     }
 
