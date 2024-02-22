81b35d59ebccc384686d4fd10afca768ba08ffe9
HuynhPhuongAnh_Nguyen <phnganhnguyn0323@gmail.com>
2024-02-22 16:29:32 +1100

    add ui

--- My project/Assets/PlayerCollision.cs
+++ My project/Assets/PlayerCollision.cs
@@ -1,12 +1,21 @@
+using System.Security.Cryptography.X509Certificates;
+using UnityEditor.Callbacks;
 using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerMovement movement;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
    void OnCollisionEnter2D(Collision2D collisionInfo) 
    {
         if (collisionInfo.collider.tag == "Obstacle")
         {
             movement.enabled = false;
+            rb.isKinematic = false;
+            Debug.Log("Game Over!");
             
         }
    }
